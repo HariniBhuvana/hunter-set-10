e2588f54b0a14ce6f6cd3ffeef0169b1d4541688
Harini <harinee2015@gmail.com>
2017-07-05 14:46:27 +0530

    Create LessenSpaces.java

---
+++ LessenSpaces.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LessenSpaces {
+
+	private static Scanner s;
+
+	public static void main(String[] args) {
+		s = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s1 = s.nextLine();
+		int len = 0;
+		String sr[] = s1.split(" ");
+		if (sr.length - 1 > 0) {
+			for (int i = 0; i < sr.length - 1; i++) {
+				len += 1;
+			}
+			System.out.println("Number of Spaces is : " + len + " for Entered String " + "/*" + s1 + "*/");
+			System.out.println("Lessen Space is : " + (len - 1) + " for Entered String " + "/*" + s1 + "*/");
+		} else {
+			System.out.println("Lessen Space is : " + (len) + " for Entered String " + "/*" + s1 + "*/");
+		}
+
+	}
+}
